de9e5f5610407f7b99ca44850317ef2170371917
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-11-08 16:51:50 -0600

    create Details component

---
+++ src/components/Details/Details.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Details extends Component {
+  // Renders the entire app on the DOM
+  render() {
+    return (
+      <div className="App">
+        <p>Details Page</p>
+        <br /> Displays movies selected from Home
+      </div>
+    );
+  }
+}
+
+export default Details;
