fba7f3cbb99f51559c9d97c622d7732e47b697a8
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-11-08 16:52:56 -0600

    create Edit component

---
+++ src/components/Edit/Edit.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Edit extends Component {
+  // Renders the entire app on the DOM
+  render() {
+    return (
+      <div className="App">
+        <p>Edit Page</p>
+        <br /> Will have input and buttons
+      </div>
+    );
+  }
+}
+
+export default Edit;
