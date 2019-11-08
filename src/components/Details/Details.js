ffdafa83116bc5d16148a8396d7348484448a179
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-11-08 17:40:05 -0600

    add back/edit buttons to Details component

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -6,6 +6,7 @@ class Details extends Component {
   render() {
     return (
       <div className="App">
+          <button>Back to List</button> <button>Edit</button>
         <p>Details Page</p>
         <br /> Displays movies selected from Home
       </div>
