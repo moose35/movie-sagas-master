e464cb7760486ed36158182950e7c7570c9ebdc5
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-11-08 17:46:47 -0600

    back button returns to Home component

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -3,10 +3,17 @@ import React, { Component } from 'react';
 
 class Details extends Component {
   // Renders the entire app on the DOM
+    goBack = (event) =>{
+        // sends user to Home component when clicked
+        event.preventDefault();
+        console.log('Back clicked');
+        this.props.history.push('/');
+    }
+
   render() {
     return (
       <div className="App">
-          <button>Back to List</button> <button>Edit</button>
+          <button onClick={this.goBack}>Back to List</button> <button>Edit</button>
         <p>Details Page</p>
         <br /> Displays movies selected from Home
       </div>
