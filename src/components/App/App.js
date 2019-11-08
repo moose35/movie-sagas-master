4c93d17ba5621b8921a1ab1f308b858c2f9d6faa
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-11-08 17:07:54 -0600

    create routers/links for navigation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import Home from '../Home/Home.js';
+import Details from '../Details/Details.js';
+import Edit from '../Edit/Edit.js';
 import './App.css';
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Home} />
+          <Route path="/details" component={Details} />
+          <Route path="/edit" component={Edit} />
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/details">Details</Link>
+            </li>
+            <li>
+              <Link to="/edit">Edit</Link>
+            </li>
+          </ul>
+        </div>
+      </Router>
     );
   }
 }
