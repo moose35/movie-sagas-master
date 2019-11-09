563850b57d61983390287fad75b655a1b4853c30
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-11-08 18:02:33 -0600

    add buttons and inputs to Edit component

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 
 
 class Edit extends Component {
-  // Renders the entire app on the DOM
-  render() {
-    return (
-      <div className="App">
-        <p>Edit Page</p>
-        <br /> Will have input and buttons
-      </div>
-    );
-  }
+    // Renders the entire app on the DOM
+    render() {
+        return (
+            <div className="App">
+                <button>Cancel</button><button>Save</button>
+                <br /><input placeholder="Movie Title" />
+                <br /><input type="text" />
+                <p>Edit Page</p>
+            </div>
+        );
+    }
 }
 
 export default Edit;
